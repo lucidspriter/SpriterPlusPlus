lucidspriter/SpriterPlusPlus

commit 750630c0edb297281f981ccd429a008198ecab6f

Added SpriteObjectInfo to ObjectFactories

spriterengine/override/objectfactory.h
@@ -30,6 +30,9 @@ namespace SpriterEngine
 
 		// TODO: Override along with TriggerObjectInfo::playTrigger() to allow for triggering actions;
 		virtual TriggerObjectInfo *newTriggerObjectInfo(std::string triggerName);
+
+		// TODO: Override along with SpriteObjectInfo::render() if you need to manage sprite as objects;
+		virtual SpriteObjectInfo *newSpriteObjectinfo();
 	};
 
 }

spriterengine/entity/object.cpp
@@ -99,6 +99,17 @@ namespace SpriterEngine
 	{
 		switch (objectType)
 		{
+		case OBJECTTYPE_SPRITE:
+			if (objectFactory)
+			{
+				entityInstanceData->setObjectInstance(objectId, name, objectFactory->newSpriteObjectinfo());
+			}
+			else
+			{
+				entityInstanceData->setObjectInstance(objectId, name, new SpriteObjectInfo());
+			}
+			break;
+
 		case OBJECTTYPE_POINT:
 			if (objectFactory)
 			{

spriterengine/override/objectfactory.cpp
@@ -4,6 +4,7 @@
 #include "../objectinfo/boneinstanceinfo.h"
 #include "../objectinfo/boxinstanceinfo.h"
 #include "../objectinfo/triggerobjectinfo.h"
+#include "../objectinfo/spriteobjectinfo.h"
 
 namespace SpriterEngine
 {
@@ -32,4 +33,8 @@ namespace SpriterEngine
 		return new TriggerObjectInfo();
 	}
 
+	SpriteObjectInfo *ObjectFactory::newSpriteObjectinfo()
+	{
+		return new SpriteObjectInfo();
+	}
 }

commit a54bcdd53b1def7a3950a5b554d15685447fee41

Fixed compilation error introduced in 750630c0edb297281f981ccd429a008198ecab6f
Missing forward declaration of SpriteObjectInfo.

Also changed __USE_PUGIXML macros in examplefilefactory to use #ifndef
instead.

example/override/examplefilefactory.cpp
@@ -4,7 +4,7 @@
 #include "../../spriterengine/override/soundfile.h"
 
 // #define __USE_PUGIXML
-#if not defined(__USE_PUGIXML)
+#ifndef __USE_PUGIXML
 #include "tinyxmlspriterfiledocumentwrapper.h"
 #else
 #include "pugixmlspriterfiledocumentwrapper.h"
@@ -33,7 +33,7 @@ namespace SpriterEngine
 
 	SpriterFileDocumentWrapper * ExampleFileFactory::newScmlDocumentWrapper()
 	{
-#if not defined(__USE_PUGIXML)
+#ifndef __USE_PUGIXML
 		return new TinyXmlSpriterFileDocumentWrapper();
 #else
 		return new PugiXmlSpriterFileDocumentWrapper();		

spriterengine/override/objectfactory.h
@@ -7,11 +7,11 @@
 
 namespace SpriterEngine
 {
-
 	class PointInstanceInfo;
 	class BoneInstanceInfo;
 	class BoxInstanceInfo;
 	class TriggerObjectInfo;
+	class SpriteObjectInfo;
 
 	class ObjectFactory
 	{